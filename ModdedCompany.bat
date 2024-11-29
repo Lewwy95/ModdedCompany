0051607ec7c349181caac5d9c119b319eb228c35
Lewwy <johnstonelewwy@gmail.com>
2024-11-29 00:00:31 +0000

    Version 4.0.0a

--- ModdedCompany.bat
+++ ModdedCompany.bat
@@ -30,10 +30,10 @@ cls
 
 :: Check For Different Version Files
 if %new% neq %current% (
-    echo Update required! Installing...
+    echo Update required! Downloading...
     timeout /t 2 /nobreak >nul
     cls
-    goto install
+    goto download
 )
 
 :: Check For Install
@@ -45,8 +45,8 @@ timeout /t 2 /nobreak >nul
 cls
 goto install
 
-:: Installation/Updater
-:install
+:: Downloader
+:download
 echo Downloading latest revision...
 echo.
 powershell -c "(New-Object System.Net.WebClient).DownloadFile('https://github.com/Lewwy95/ModdedCompany/archive/refs/heads/main.zip','%~dp0\bin\Temp\ModdedCompany-main.zip')"
@@ -55,20 +55,6 @@ cls
 :: Extract Latest Revision
 echo Extracting latest revision...
 powershell -c "Expand-Archive '%~dp0\bin\Temp\ModdedCompany-main.zip' -Force '%~dp0\bin\Temp'"
-cls
-
-:: Clear Mods Folder
-echo Clearing mods folder...
-del /s /q "%~dp0\bin\Mods\*"
-rmdir /s /q "%~dp0\bin\Mods"
-mkdir "%~dp0\bin\Mods"
-cls
-
-:: Clear Configs Folder
-echo Clearing configs folder...
-del /s /q "%~dp0\bin\Configs\*"
-rmdir /s /q "%~dp0\bin\Configs"
-mkdir "%~dp0\bin\Configs"
 cls
 
 :: Deploy Latest Revision
@@ -85,26 +71,24 @@ cls
 call "%~dp0\Uninstall.bat"
 
 :: Move New Mods
+:install
 echo Installing mods...
 if not exist "%~dp0..\BepInEx" (
     :: Create the core folders and move the files
     powershell -c "Expand-Archive '%~dp0\bin\Mods\BepInEx.zip' -Force '%~dp0\bin\Temp'"
     xcopy /s /y /i "%~dp0\bin\Temp\BepInEx\BepInEx\*" "%~dp0..\BepInEx"
-    timeout /t 2 /nobreak >nul
 
     :: Make unique mod and dependency folders
     mkdir "%~dp0..\BepInEx\patchers\FixPluginTypesSerialization"
-    mkdir "%~dp0..\BepInEx\config\MoreSuitsConfig"
-    mkdir "%~dp0..\BepInEx\plugins\moresuits"
+    mkdir "%~dp0..\BepInEx\plugins\resAdditionalSuits"
     mkdir "%~dp0..\BepInEx\plugins\Assets"
     mkdir "%~dp0..\BepInEx\plugins\LethalResonance"
     mkdir "%~dp0..\BepInEx\plugins\CSync"
-    timeout /t 2 /nobreak >nul
 
     :: Copy mods and dependencies over
     xcopy /s /y /i "%~dp0\bin\Configs\*" "%~dp0..\BepInEx\config"
     xcopy /s /y /i "%~dp0\bin\Mods\*.dll" "%~dp0..\BepInEx\plugins"
-    xcopy /s /y /i "%~dp0\bin\Mods\moresuits\*" "%~dp0..\BepInEx\plugins\moresuits"
+    xcopy /s /y /i "%~dp0\bin\Mods\resAdditionalSuits\*" "%~dp0..\BepInEx\plugins\resAdditionalSuits"
     xcopy /s /y /i "%~dp0\bin\Mods\Assets\*" "%~dp0..\BepInEx\plugins\Assets"
     xcopy /s /y /i "%~dp0\bin\Mods\LethalResonance\*" "%~dp0..\BepInEx\plugins\LethalResonance"
     xcopy /s /y /i "%~dp0\bin\Mods\CSync\*" "%~dp0..\BepInEx\plugins\CSync"
@@ -138,6 +122,7 @@ cls
 :modlist
 if exist "%~dp0\modlist.txt" del /s /q "%~dp0\modlist.txt"
 echo Creating mods text file...
+echo - AdditionalSuits>> modlist.txt
 echo - CustomDeathPenalty>> modlist.txt
 echo - FasterItemDropship>> modlist.txt
 echo - Helmet Cameras>> modlist.txt
@@ -148,7 +133,6 @@ echo - LETHALRESONANCE>> modlist.txt
 echo - Mimics>> modlist.txt
 echo - MoreBlood>> modlist.txt
 echo - MoreCompany>> modlist.txt
-echo - More Suits>> modlist.txt
 echo - QuickRestart>> modlist.txt
 echo - QuotaRollover>> modlist.txt
 echo - ReservedFlashlightSlot>> modlist.txt
